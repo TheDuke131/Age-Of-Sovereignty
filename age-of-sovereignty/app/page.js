81e90ba6f2964a0998f3e5127706fff1d6b5ee58
TheDuke131 <jwwgames065@gmail.com>
2026-02-13 05:23:32 +0000

    message

---
+++ age-of-sovereignty/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>My Game Website</h1>
+      <p>This is my first site running on GitHub Codespaces.</p>
+    </main>
+  );
+}
